6a4402032930f02a917ada6aa2fa614afdd42b83
Tomas Petricek <tomas@tomasp.net>
2016-08-21 01:52:30 +0100

    Azure host wrapper for FsAutoComplete Suave

---
+++ build.fsx
@@ -0,0 +1,61 @@
+// --------------------------------------------------------------------------------------
+// FAKE build script
+// --------------------------------------------------------------------------------------
+
+#r "packages/FAKE/tools/FakeLib.dll"
+open Fake
+open System
+open System.IO
+open FSharp.Data
+
+let localPort = 7101
+let release = "paket-files/github.com/ionide/FsAutoComplete/bin/release"
+System.Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
+
+// --------------------------------------------------------------------------------------
+// For local run - automatically reloads scripts
+// --------------------------------------------------------------------------------------
+
+let startServer () = 
+  ExecProcessWithLambdas
+    (fun info -> 
+        info.FileName <- System.IO.Path.GetFullPath(release </> "FsAutoComplete.Suave.exe")
+        info.Arguments <- string localPort
+        info.WorkingDirectory <- __SOURCE_DIRECTORY__)
+    TimeSpan.MaxValue false ignore ignore 
+
+Target "run" (fun _ ->
+  async { return startServer() }  |> Async.Ignore |> Async.Start
+  traceImportant "FsAutoComplete server started...."
+  traceImportant "Press Enter to stop!"
+  Console.ReadLine() |> ignore
+)
+
+// --------------------------------------------------------------------------------------
+// Azure - deploy copies the binary to wwwroot/bin
+// --------------------------------------------------------------------------------------
+
+let newName prefix f = 
+  Seq.initInfinite (sprintf "%s_%d" prefix) |> Seq.skipWhile (f >> not) |> Seq.head
+
+Target "deploy" (fun _ ->
+  // Pick a subfolder that does not exist
+  let wwwroot = "../wwwroot"
+  let subdir = newName "deploy" (fun sub -> not (Directory.Exists(wwwroot </> sub)))
+  
+  // Deploy everything into new empty folder
+  let deployroot = wwwroot </> subdir
+  CleanDir deployroot
+  CleanDir (deployroot </> "bin")
+  CopyRecursive release (deployroot </> "bin") false |> ignore
+  
+  let config = File.ReadAllText("web.config").Replace("%DEPLOY_SUBDIRECTORY%", subdir)
+  File.WriteAllText(wwwroot </> "web.config", config)
+
+  // Try to delete previous folders, but ignore failures
+  for dir in Directory.GetDirectories(wwwroot) do
+    if Path.GetFileName(dir) <> subdir then 
+      try CleanDir dir; DeleteDir dir with _ -> ()
+)
+
+RunTargetOrDefault "run"
